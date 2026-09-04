0bd5cd376645a66d39eb2b1ced6da7183ffa86c6
Esteban Alfaro <esteban@sentientagency.io>
2026-09-03 22:34:17 -0600

    Revert "deploy: cut traffic to diskless API"
    This reverts commit 0f971506ddc94dd672eb262db99fafab5deffdaa.

--- assets/dashboard-CztQ-bxA.js
+++
@@ -1,1 +0,0 @@
-import{R as e,j as t,a as r}from"./urlCodec-D42f2PuB.js";import{A as o}from"./styles-GCoiwIMW.js";import"./traselveloreal-profile-BQlVL52s.js";import"./search-qbivrh0Y.js";e.createRoot(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(o,{})}));
