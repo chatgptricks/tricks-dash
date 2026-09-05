c1f85cda0f6c3006e59ebae3add0f129b4296135
Esteban Alfaro <esteban@sentientagency.io>
2026-09-05 02:06:47 -0600

    Revert "Publish unified navigation and topic champions"
    This reverts commit 8dbe0d3ac98f50f82f8839f8578f7ffd8bbab9b9.

--- assets/main-DFezjAAK.js
+++
@@ -1,1 +0,0 @@
-import{R as e,j as t,a as r}from"./queueLive-D8pKFDap.js";import{A as o}from"./styles-BH90rx5c.js";e.createRoot(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(o,{})}));
