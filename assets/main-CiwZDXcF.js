fe0afd1eeafe65c615f8ad4b4ba8b7171fdf7262
Esteban Alfaro <esteban@sentientagency.io>
2026-09-05 02:31:37 -0600

    Publish role preview and progressive grouping

---
+++ assets/main-CiwZDXcF.js
@@ -0,0 +1,1 @@
+import{R as e,j as t,a as r}from"./queueLive-D8pKFDap.js";import{A as o}from"./styles-DnXMV_Oj.js";e.createRoot(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(o,{})}));
