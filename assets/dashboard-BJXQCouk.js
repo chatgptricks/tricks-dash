8a8eec9fae13862763ea188e3f40ed255a6b4242
Esteban Alfaro <esteban@sentientagency.io>
2026-09-15 18:33:12 -0600

    Publish caption regeneration fix

--- assets/dashboard-BJXQCouk.js
+++
@@ -1,1 +0,0 @@
-import{R as e,j as t,a as r}from"./urlCodec-rfzUNFwg.js";import{A as o}from"./styles-HTU5zPH3.js";e.createRoot(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(o,{})}));
