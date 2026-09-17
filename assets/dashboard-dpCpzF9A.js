bfbcb37eff1905e59d3db5f1d49a6e3aa2db05f1
Esteban Alfaro <esteban@sentientagency.io>
2026-09-16 19:34:58 -0600

    Publish restored grouping gestures

--- assets/dashboard-dpCpzF9A.js
+++
@@ -1,1 +0,0 @@
-import{R as e,j as t,a as r}from"./urlCodec-BCR1ThTl.js";import{A as o}from"./styles-CVR-sojC.js";e.createRoot(document.getElementById("root")).render(t.jsx(r.StrictMode,{children:t.jsx(o,{})}));
